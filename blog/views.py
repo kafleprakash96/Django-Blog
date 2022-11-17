23abbd5d9575dd34647aebc59b6c32dace1a6a98
Prakash Kafle <kafleprakash96@gmail.com>
2022-11-16 18:52:41 -0600

    Updated project with about page

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> Blog Home </>')
+    return HttpResponse("<h1> Blog Home </>")
 
+
+def about(request):
+    return HttpResponse("<h1> About Page </>")
