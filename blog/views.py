52b0631976911b1cf1d7c01b90026b370c2a70f4
Prakash Kafle <kafleprakash96@gmail.com>
2022-11-16 20:52:14 -0600

    Used bootstrap in base.html

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        "author": "Prakash Kafle",
+        "title": "Blog 1",
+        "content": "First Post Content",
+        "date_posted": "11/16/2022",
+    },
+    {
+        "author": "Dipika Bhandari",
+        "title": "Blog 2",
+        "content": "Second Post Content",
+        "date_posted": "11/15/2022",
+    },
+]
+
 
 # Create your views here.
 def home(request):
-    return render(request, "blog/home.html")
+    context = {"posts": posts, "title": "home"}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, "blog/about.html")
+    return render(request, "blog/about.html", {"title": "about"})
