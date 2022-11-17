d20e5f1bb72ac72c712e717ad400b6613443005d
Prakash Kafle <kafleprakash96@gmail.com>
2022-11-17 14:36:45 -0600

    Updated settings.py for media

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
