ce6a075d3fb41d1ac6f20432d21843d5ff778827
Prakash Kafle <kafleprakash96@gmail.com>
2022-11-17 03:19:06 -0600

    Created profile.html and updated base.html,settings.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
         auth_views.LogoutView.as_view(template_name="users/logout.html"),
         name="logout",
     ),
+    path("profile/", user_views.profile, name="profile"),
 ]
