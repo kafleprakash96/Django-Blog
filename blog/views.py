92f330dd534d37d4c2b986f0515db3997c6040e4
Prakash Kafle <kafleprakash96@gmail.com>
2022-11-16 22:39:46 -0600

    Created model for database and updated admin.py

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        "author": "Prakash Kafle",
-        "title": "Blog 1",
-        "content": "First Post Content",
-        "date_posted": "11/16/2022",
-    },
-    {
-        "author": "Dipika Bhandari",
-        "title": "Blog 2",
-        "content": "Second Post Content",
-        "date_posted": "11/15/2022",
-    },
-]
+from .models import Post
 
 
 # Create your views here.
 def home(request):
-    context = {"posts": posts, "title": "home"}
+    context = {"posts": Post.objects.all(), "title": "home"}
     return render(request, "blog/home.html", context)
 
 
