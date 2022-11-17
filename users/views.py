6438182d6ae7a8183a45f8deed4cf9b281f10428
Prakash Kafle <kafleprakash96@gmail.com>
2022-11-17 17:07:42 -0600

    Updated signals.py and profile

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UpdateProfileForm, UserUpdatedForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -19,4 +19,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        u_form = UserUpdatedForm(request.POST, instance=request.user)
+        p_form = UpdateProfileForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Acoount has been updated!")
+            return redirect("profile")
+    else:
+        u_form = UserUpdatedForm(instance=request.user)
+        p_form = UpdateProfileForm(instance=request.user.profile)
+
+    context = {"u_form": u_form, "p_form": p_form}
+    return render(request, "users/profile.html", context)
