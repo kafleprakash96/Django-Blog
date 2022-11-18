519d20650cf3f626979c97bb2a41c2cdce902ade
Prakash Kafle <kafleprakash96@gmail.com>
2022-11-18 02:10:31 -0600

    Added json data and worked on pagination

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm, UpdateProfileForm, UserUpdatedForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdatedForm, UpdateProfileForm
 
 
 def register(request):
@@ -10,7 +10,9 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}!")
+            messages.success(
+                request, f"Your account has been created! You are now able to log in"
+            )
             return redirect("login")
     else:
         form = UserRegisterForm()
@@ -24,15 +26,16 @@ def profile(request):
         p_form = UpdateProfileForm(
             request.POST, request.FILES, instance=request.user.profile
         )
-
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f"Acoount has been updated!")
+            messages.success(request, f"Your account has been updated!")
             return redirect("profile")
+
     else:
         u_form = UserUpdatedForm(instance=request.user)
         p_form = UpdateProfileForm(instance=request.user.profile)
 
     context = {"u_form": u_form, "p_form": p_form}
+
     return render(request, "users/profile.html", context)
